363baed5b1d66ce00e909a317a848bd31f935f7c
andrii <duschado@gmail.com>
2018-11-26 00:14:03 +0200

    Get conditions from accuweather

--- weatherapp.py
+++ weatherapp.py
@@ -12,7 +12,7 @@ ACCU_URL = "https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/32456
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 accu_request = Request(ACCU_URL, headers=headers)
 accu_page = urlopen(accu_request).read()
-accu_page = str(accu_page)
+accu_page = accu_page.decode('utf-8')
 
 ACCU_TEMP_TAG = '<span class="large-temp">'
 accu_temp_tag_size = len(ACCU_TEMP_TAG)
@@ -25,5 +25,18 @@ for char in accu_page[accu_temp_value_start:]:
 	else:
 		break
 
+ACCU_COND_TAG = '<span class="cond">'
+accu_cond_tag_size = len(ACCU_COND_TAG)
+accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
+accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
+accu_cond = ''
+for char in accu_page[accu_cond_value_start:]:
+	if char != '<':
+		accu_cond += char
+	else:
+		break
+	
+
 print('AccuWeather: \n')
 print(f'Temperature: {html.unescape(accu_temp)}\n')
+print(f'Conditions: {accu_cond}\n')

5f9ef456a2236666123a475458df0114dc092433
andrii <duschado@gmail.com>
2018-12-09 00:42:54 +0200

    Rewrite the code using the functions

--- weatherapp.py
+++ weatherapp.py
@@ -7,72 +7,94 @@ import html
 from urllib.request import urlopen, Request
 
 ACCU_URL = "https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561"
+ACCU_TAGS = ('<span class="large-temp">', '<span class="cond">')
 
-# getting page from server
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
-accu_request = Request(ACCU_URL, headers=headers)
-accu_page = urlopen(accu_request).read()
-accu_page = accu_page.decode('utf-8')
-
-ACCU_TEMP_TAG = '<span class="large-temp">'
-accu_temp_tag_size = len(ACCU_TEMP_TAG)
-accu_temp_tag_index = accu_page.find(ACCU_TEMP_TAG)
-accu_temp_value_start = accu_temp_tag_index + accu_temp_tag_size
-accu_temp = ''
-for char in accu_page[accu_temp_value_start:]:
-    if char != '<':
-        accu_temp += char
-    else:
-        break
-
-ACCU_COND_TAG = '<span class="cond">'
-accu_cond_tag_size = len(ACCU_COND_TAG)
-accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
-accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
-accu_cond = ''
-for char in accu_page[accu_cond_value_start:]:
-    if char != '<':
-        accu_cond += char
-    else:
-        break
+#RP5_URL = ("http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D1%83_%D0%9B%D1"
+#           "%8C%D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1"
+#          "%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C")
 
 
-print('AccuWeather: \n')
-print(f'Temperature: {html.unescape(accu_temp)}\n')
-print(f'Conditions: {accu_cond}\n')
+#RP5_TEMP_TAG = '<span class="t_0" style="display: block;">'
+
+#RP5_CONTAINER_TAG = '<div class="cc_0">'
+#RP5_COND_TAG = """<div class="cn4" onmouseover="tooltip(this, '<b>"""
+#rp5_cond_tag_size = len(RP5_COND_TAG)
+#rp5_cond_tag_index = rp5_page.find(RP5_COND_TAG, rp5_page.find(RP5_CONTAINER_TAG))
+#rp5_cond_value_start = rp5_cond_tag_index + rp5_cond_tag_size
+#rp5_cond = ''
+#for char in rp5_page[rp5_cond_value_start:]:
+#    if char != '<':
+#        rp5_cond += char
+#    else:
+#        break
 
 
-RP5_URL = ("http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D1%83_%D0%9B%D1"
-           "%8C%D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1"
-           "%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C")
 
-rp5_request = Request(RP5_URL, headers=headers)
-rp5_page = urlopen(rp5_request).read()
-rp5_page = rp5_page.decode('utf-8')
+def get_request_headers():
+    return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 
-RP5_TEMP_TAG = '<span class="t_0" style="display: block;">'
-rp5_temp_tag_size = len(RP5_TEMP_TAG)
-rp5_temp_tag_index = rp5_page.find(RP5_TEMP_TAG)
-rp5_temp_value_start = rp5_temp_tag_index + rp5_temp_tag_size
-rp5_temp = ''
-for char in rp5_page[rp5_temp_value_start:]:
-    if char != '<':
-        rp5_temp += char
-    else:
-        break
+def get_page_source(url):
+    """
+    """
 
-RP5_CONTAINER_TAG = '<div class="cc_0">'
-RP5_COND_TAG = """<div class="cn4" onmouseover="tooltip(this, '<b>"""
-rp5_cond_tag_size = len(RP5_COND_TAG)
-rp5_cond_tag_index = rp5_page.find(RP5_COND_TAG, rp5_page.find(RP5_CONTAINER_TAG))
-rp5_cond_value_start = rp5_cond_tag_index + rp5_cond_tag_size
-rp5_cond = ''
-for char in rp5_page[rp5_cond_value_start:]:
-    if char != '<':
-        rp5_cond += char
-    else:
-        break
+    request = Request(url, headers=get_request_headers())
+    page_source = urlopen(request).read()
+    return page_source.decode('utf-8')
 
-print('RP5: \n')
-print(f'Temperature: {html.unescape(rp5_temp)}\n')
-print(f'Conditions: {rp5_cond}\n')
+def get_tag_content(page_content, tag):
+    """
+    """
+    print(tag)
+    tag_index = page_content.find(tag)
+    tag_size = len(tag)
+    value_start = tag_index + tag_size
+
+    content = ''
+    for c in page_content[value_start:]:
+        if c != '<':
+            content += c
+        else:
+            break
+    return content
+
+
+def get_weather_info(page_content, tags):
+    """
+    """
+
+    return tuple([get_tag_content(page_content, tag) for tag in tags])
+
+def produce_output(provider_name, temp, condition):
+    """
+    """
+
+    print(f'\n{provider_name}:')
+    print(f'Temperature: {html.unescape(temp)}\n')
+    print(f'Condition: {condition}\n')
+
+
+def main():
+    """Main entry point.
+    """
+
+    weather_sites = {"AccuWeather": (ACCU_URL, ACCU_TAGS)} #,
+#                     "RP5": (RP5_URL, RP5_TAGS)}
+    for name in weather_sites:
+        url, tags = weather_sites[name]
+        content = get_page_source(url)
+        temp, condition = get_weather_info(content, tags)
+        produce_output(name, temp, condition)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
